322cd7efe7e463db3cc2ca75a820f2a08e081382
PHANIT-VONG <vphanit2020@gmail.com>
2022-02-11 10:07:11 +0700

    Add Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_test/controllers/bindings/auth_binding.dart';
 import 'package:firebase_test/controllers/theme_controller.dart';
-import 'package:firebase_test/views/pages/google_login.dart';
+import 'package:firebase_test/views/pages/notification_page.dart';
 import 'package:firebase_test/views/pages/root_page.dart';
 import 'package:firebase_test/views/widgets/themes.dart';
 import 'package:flutter/material.dart';
@@ -9,10 +9,16 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  await GetStorage.init();
+  //await GetStorage.init();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  await init();
+  runApp(const MyApp());
+}
+
+Future init() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,16 +26,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _themeController = Get.put<ThemeController>(ThemeController());
-    return GetMaterialApp(
-      initialBinding: AuthBinding(),
+    //final _themeController = Get.put<ThemeController>(ThemeController());
+    return const MaterialApp(
+      //initialBinding: AuthBinding(),
       debugShowCheckedModeBanner: false,
-      themeMode: _themeController.theme,
-      theme: Themes.lightTheme,
-      darkTheme: Themes.darkTheme,
+      // themeMode: _themeController.theme,
+      //theme: Themes.lightTheme,
+      //darkTheme: Themes.darkTheme,
       title: 'Flutter Firebase Test',
+      home: NotificationPage(),
       //home: const RootPage(),
-      home: GoogleLogin(),
+      //home: GoogleLogin(),
     );
   }
 }
